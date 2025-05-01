874670bd7062c1dd5f49fd97b528ba29850b5e93
A Corporação <209138335+ACorporacao@users.noreply.github.com>
2025-05-01 12:42:43 -0300

    Chimpanzini fix

--- script/c302.lua
+++ script/c302.lua
@@ -1,38 +1,34 @@
--- Chimpanzini Bananini
-local c302 = {}
+local s, id = GetID()
 
-c302.initial_effect = function(c)
+function s.initial_effect(c)
 	-- Invocação-Especial da mão se apenas o oponente controlar monstros
 	local e1 = Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(302, 0)) -- Descrição do efeito (use um ID único)
+	e1:SetDescription(aux.Stringid(id, 0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_IGNITION) -- Ativável manualmente
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c302.spcon) -- Verifica a condição
-	e1:SetTarget(c302.sptg) -- Define o alvo
-	e1:SetOperation(c302.spop) -- Executa a operação
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
 
--- Condição: Somente o oponente controla monstros
-function c302.spcon(e, tp, eg, ep, ev, re, r, rp)
-	return Duel.GetFieldGroupCount(tp, LOCATION_MZONE, 0) == 0 -- Você não tem monstros
-		and Duel.GetFieldGroupCount(1-tp, LOCATION_MZONE, 0) > 0 -- O oponente tem pelo menos 1
+function s.spcon(e, tp, eg, ep, ev, re, r, rp)
+	return Duel.GetFieldGroupCount(tp, LOCATION_MZONE, 0) == 0
+		and Duel.GetFieldGroupCount(1-tp, LOCATION_MZONE, 0) > 0
 end
 
--- Alvo: Permitir Invocação-Especial
-function c302.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
-	if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
-		and e:GetHandler():IsCanBeSpecialSummoned(e, 0, tp, false, false) end
+function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk == 0 then
+		return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
+			and e:GetHandler():IsCanBeSpecialSummoned(e, 0, tp, false, false)
+	end
 	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
 end
 
--- Operação: Invocar o monstro
-function c302.spop(e, tp, eg, ep, ev, re, r, rp)
+function s.spop(e, tp, eg, ep, ev, re, r, rp)
 	local c = e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)
 	end
 end
-
-return c302
