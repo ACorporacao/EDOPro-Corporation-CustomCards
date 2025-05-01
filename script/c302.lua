febb2dacd85ff82bfb9ba46d65cf987e07fd0ce2
A Corporação <209138335+ACorporacao@users.noreply.github.com>
2025-05-01 12:47:00 -0300

    Fix 2 Chimpanzini

--- script/c302.lua
+++ script/c302.lua
@@ -1,11 +1,12 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
-	-- Invocação-Especial da mão se apenas o oponente controlar monstros
+	-- Invocação-Especial automática (sem botão de ativação)
 	local e1 = Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id, 0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetType(EFFECT_TYPE_QUICK_O) -- Tipo mais adequado para resposta rápida
+	e1:SetCode(EVENT_FREE_CHAIN) -- Pode ser ativado a qualquer momento
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
@@ -14,8 +15,10 @@ function s.initial_effect(c)
 end
 
 function s.spcon(e, tp, eg, ep, ev, re, r, rp)
+	-- Só pode ser Invocado se o oponente controlar um monstro e você não
 	return Duel.GetFieldGroupCount(tp, LOCATION_MZONE, 0) == 0
 		and Duel.GetFieldGroupCount(1-tp, LOCATION_MZONE, 0) > 0
+		and Duel.GetCurrentPhase() == PHASE_MAIN1 or Duel.GetCurrentPhase() == PHASE_MAIN2 -- Só na Main Phase
 end
 
 function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
