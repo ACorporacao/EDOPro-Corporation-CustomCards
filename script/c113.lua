4cf176c386d75ab0ad3b7692eec452367de8d871
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-05 16:03:46 -0300

    Update c113.lua

--- script/c113.lua
+++ script/c113.lua
@@ -1,84 +1,54 @@
---(DEV)Titan de Ataque
+-- Titan Assaltante Eren Yeager
 local s,id = GetID()
 function s.initial_effect(c)
-	-- Restrição de Invocação Xyz
+	-- Só pode ser Invocado por Invocação-Xyz (manual)
 	c:EnableReviveLimit()
-	Xyz.AddProcedure(c,s.matfilter,4,1) -- Nível 4, 1 material
-end
-
--- Filtro para material Xyz (Eren Yeager que não foi Invocado Normalmente neste turno)
-function s.matfilter(c)
-	return c:IsCode(101) and not c:IsSummonType(SUMMON_TYPE_NORMAL)
-end
-
--- EFEITO 01: Perfuração
-function s.initial_effect(c)
-	-- ...
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_PIERCE)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetCondition(s.xyzcon)
+	e1:SetOperation(s.xyzop)
+	e1:SetValue(SUMMON_TYPE_XYZ)
 	c:RegisterEffect(e1)
-
-	-- EFEITO 02: Após 3 Fases Finais do oponente, invocar Eren Yeager
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PHASE+PHASE_END)
-	e2:SetCondition(s.spcon)
-	e2:SetOperation(s.spop)
-	e2:SetCountLimit(1)
-	e2:SetRange(LOCATION_MZONE)
-	c:RegisterEffect(e2)
-
-	-- EFEITO 03: Autodestruição se não tiver Eren Yeager como material
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_ADJUST)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(s.descon)
-	e3:SetOperation(s.desop)
-	c:RegisterEffect(e3)
 end
 
--- EFEITO 02: Contador de turnos e invocação de Eren Yeager
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetTurnPlayer()==tp then return false end
-	if not c:IsSummonType(SUMMON_TYPE_XYZ) then return false end
+-- Filtro para Eren Yeager (c101)
+function s.matfilter(c)
+	return c:IsFaceup() and c:IsCode(101) and s.checkErenCondition(c)
+end
 
-	if not c:GetFlagEffectLabel(id) then
-		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,3)
-		return false
+-- Condição personalizada
+function s.checkErenCondition(c)
+	local turn_id = Duel.GetTurnCount()
+	local summon_turn = c:GetTurnID()
+	local summon_type = c:GetSummonType()
+	
+	-- Se foi Invocado Especialmente, pode sempre
+	if (summon_type & SUMMON_TYPE_SPECIAL) == SUMMON_TYPE_SPECIAL then
+		return true
 	end
 
-	local ct=c:GetFlagEffectLabel(id)
-	if ct and ct<2 then
-		c:SetFlagEffectLabel(id,ct+1)
-		return false
+	-- Se foi Invocado Normalmente, só se não for neste turno
+	if (summon_type & SUMMON_TYPE_NORMAL) == SUMMON_TYPE_NORMAL then
+		return summon_turn < turn_id
 	end
 
-	return true
+	return false
 end
 
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local og=c:GetOverlayGroup()
-	local yg=og:Filter(Card.IsCode,nil,101)
-	if #yg>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tc=yg:Select(tp,1,1,nil):GetFirst()
-		c:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-	end
+-- Condição de Invocação Xyz
+function s.xyzcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 
--- EFEITO 03: Destruição se não houver Eren Yeager entre os materiais
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local og=c:GetOverlayGroup()
-	return og:FilterCount(Card.IsCode,nil,101)==0
-end
-
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	Duel.Destroy(c,REASON_EFFECT)
+-- Operação da Invocação Xyz
+function s.xyzop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local g=Duel.SelectMatchingCard(tp,s.matfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	c:SetMaterial(g)
+	Duel.Overlay(c,g)
 end
