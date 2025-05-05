7777d43f6c93ee77f9c7ed9c00467546d9b8b634
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-05 15:55:16 -0300

    Update c113.lua

--- script/c113.lua
+++ script/c113.lua
@@ -3,7 +3,7 @@ local s,id = GetID()
 function s.initial_effect(c)
 	-- Restrição de Invocação Xyz
 	c:EnableReviveLimit()
-	aux.AddXyzProcedure(c,s.matfilter,4,1) -- Nível 4, 1 material
+	Xyz.AddProcedure(c,s.matfilter,4,1) -- Nível 4, 1 material
 end
 
 -- Filtro para material Xyz (Eren Yeager que não foi Invocado Normalmente neste turno)
