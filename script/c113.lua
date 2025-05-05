dc86fe514283f2975f0043924efeb16061b64e68
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-05 18:47:49 -0300

    k

--- script/c113.lua
+++ script/c113.lua
@@ -1,7 +1,7 @@
 -- Titan Assaltante Eren Yeager
 local s,id = GetID()
 function s.initial_effect(c)
-	-- Só pode ser Invocado por Invocação-Xyz (manual)
+	-- Invocação personalizada por Xyz
 	c:EnableReviveLimit()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -13,48 +13,38 @@ function s.initial_effect(c)
 	e1:SetValue(SUMMON_TYPE_XYZ)
 	c:RegisterEffect(e1)
 
-	-- EFEITO 02: Após 3 Fases Finais do oponente, Invoca Especialmente o Eren Yeager" usado como material
+	-- Iniciar contagem após Invocação por Xyz
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PHASE+PHASE_END)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(s.spcon)
-	e2:SetOperation(s.spop)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) end)
+	e2:SetOperation(s.start_count)
 	c:RegisterEffect(e2)
 end
 
--- Filtro para Eren Yeager (c101)
+-- ===== INVOCACAO XYZ CUSTOMIZADA =====
+
 function s.matfilter(c)
 	return c:IsFaceup() and c:IsCode(101) and s.checkErenCondition(c)
 end
 
--- Condição personalizada
 function s.checkErenCondition(c)
 	local turn_id = Duel.GetTurnCount()
 	local summon_turn = c:GetTurnID()
 	local summon_type = c:GetSummonType()
-	
-	-- Se foi Invocado Especialmente, pode sempre
-	if (summon_type & SUMMON_TYPE_SPECIAL) == SUMMON_TYPE_SPECIAL then
-		return true
-	end
-
-	-- Se foi Invocado Normalmente, só se não for neste turno
+	if (summon_type & SUMMON_TYPE_SPECIAL) == SUMMON_TYPE_SPECIAL then return true end
 	if (summon_type & SUMMON_TYPE_NORMAL) == SUMMON_TYPE_NORMAL then
 		return summon_turn < turn_id
 	end
-
 	return false
 end
 
--- Condição de Invocação Xyz
 function s.xyzcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 
--- Operação da Invocação Xyz
 function s.xyzop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
 	local g=Duel.SelectMatchingCard(tp,s.matfilter,tp,LOCATION_MZONE,0,1,1,nil)
@@ -62,28 +52,44 @@ function s.xyzop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Overlay(c,g)
 end
 
--- ===== EFEITO 02: Invocação automática do Eren após 3 Fases Finais do oponente =====
+-- ===== CONTAGEM DE FASE FINAL DO OPONENTE =====
 
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+function s.start_count(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	-- Contamos apenas Fases Finais do oponente
-	if Duel.GetTurnPlayer()~=tp then
-		local ct=c:GetFlagEffectLabel(id) or 0
-		ct=ct+1
-		c:ResetFlagEffect(id)
-		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
-		c:SetFlagEffectLabel(id,ct)
-		return ct==3
-	end
-	return false
+	c:SetTurnCounter(0)
+
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.count_con)
+	e1:SetOperation(s.count_op)
+	e1:SetReset(RESETS_STANDARD_PHASE_END+RESET_OPPO_TURN,3)
+	c:RegisterEffect(e1)
+
+	-- Usado para garantir reset da contagem
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetCode(1082946)
+	e2:SetLabelObject(e1)
+	e2:SetOwnerPlayer(tp)
+	e2:SetOperation(s.reset)
+	e2:SetReset(RESETS_STANDARD_PHASE_END+RESET_OPPO_TURN,3)
+	c:RegisterEffect(e2)
 end
 
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local og=c:GetOverlayGroup()
-	local eren=og:Filter(Card.IsCode,nil,101):GetFirst()
-	if eren and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.Overlay(c,og:Filter(aux.NOT(Card.IsCode),nil,101)) -- Remove outros materiais se houver
-		Duel.SpecialSummon(eren,0,tp,tp,false,false,POS_FACEUP)
-	end
+function s.count_con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
 end
+
+function s.count_op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ct=c:GetTurnCounter()+1
+	c:SetTurnCounter(ct)
+	if ct==3 then
+		-- Invoca Especialmente "Eren Yeager" do overlay
+		local og=c:GetOverlayGroup()
+		local eren=og:Filter(Card.IsCode,nil,101):GetFirst()
