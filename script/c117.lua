035aa872d471cbb7bf5e508b90e436f4cb62aaa8
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-05 20:10:02 -0300

    AoT count Fix

--- script/c117.lua
+++ script/c117.lua
@@ -1,7 +1,7 @@
---(DEV)Titan Blindado
+-- Titan Assaltante Eren Yeager
 local s,id = GetID()
 function s.initial_effect(c)
-	-- Só pode ser Invocado por Invocação-Xyz (manual)
+	-- Invocação personalizada por Xyz
 	c:EnableReviveLimit()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -12,43 +12,135 @@ function s.initial_effect(c)
 	e1:SetOperation(s.xyzop)
 	e1:SetValue(SUMMON_TYPE_XYZ)
 	c:RegisterEffect(e1)
+
+	-- Iniciar contagem após Invocação por Xyz
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) end)
+	e2:SetOperation(s.start_count)
+	c:RegisterEffect(e2)
+
+	-- EFEITO 03: Se não tiver Eren como material, destrua no fim do turno
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.no_eren_cond)
+	e3:SetOperation(s.destroy_self)
+	c:RegisterEffect(e3)
+
+
 end
 
--- Filtro para Eren Yeager (c101)
+-- ===== INVOCACAO XYZ CUSTOMIZADA =====
+
 function s.matfilter(c)
 	return c:IsFaceup() and c:IsCode(106) and s.checkErenCondition(c)
 end
 
--- Condição personalizada
 function s.checkErenCondition(c)
 	local turn_id = Duel.GetTurnCount()
 	local summon_turn = c:GetTurnID()
 	local summon_type = c:GetSummonType()
-	
-	-- Se foi Invocado Especialmente, pode sempre
-	if (summon_type & SUMMON_TYPE_SPECIAL) == SUMMON_TYPE_SPECIAL then
-		return true
-	end
-
-	-- Se foi Invocado Normalmente, só se não for neste turno
+	if (summon_type & SUMMON_TYPE_SPECIAL) == SUMMON_TYPE_SPECIAL then return true end
 	if (summon_type & SUMMON_TYPE_NORMAL) == SUMMON_TYPE_NORMAL then
 		return summon_turn < turn_id
 	end
-
 	return false
 end
 
--- Condição de Invocação Xyz
 function s.xyzcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 
--- Operação da Invocação Xyz
 function s.xyzop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
 	local g=Duel.SelectMatchingCard(tp,s.matfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	c:SetMaterial(g)
 	Duel.Overlay(c,g)
 end
+
+-- ===== CONTAGEM DE FASE FINAL DO OPONENTE =====
+
+function s.start_count(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	c:SetTurnCounter(0)
+
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.count_con)
+	e1:SetOperation(s.count_op)
+	e1:SetReset(RESETS_STANDARD_PHASE_END+RESET_OPPO_TURN,3)
+	c:RegisterEffect(e1)
+
+	-- Usado para garantir reset da contagem
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetCode(1082946)
+	e2:SetLabelObject(e1)
+	e2:SetOwnerPlayer(tp)
+	e2:SetOperation(s.reset)
+	e2:SetReset(RESETS_STANDARD_PHASE_END+RESET_OPPO_TURN,3)
+	c:RegisterEffect(e2)
+end
+
+function s.count_con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+
+function s.count_op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ct=c:GetTurnCounter()+1
+	c:SetTurnCounter(ct)
+	if ct==3 then
+		-- Verifica se há espaço no campo antes de invocar o Eren
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then
+			Duel.Destroy(c,REASON_RULE)
+			return
+		end
+
+		-- Invoca Especialmente "Eren Yeager" do overlay
+		local og=c:GetOverlayGroup()
+		local eren=og:Filter(Card.IsCode,nil,106):GetFirst()
+		if eren then
+			Duel.Overlay(c,og:Filter(aux.NOT(Card.IsCode),nil,106)) -- remove os outros materiais
+			Duel.SpecialSummon(eren,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+
+
+function s.reset(e,tp,eg,ep,ev,re,r,rp)
+	s.count_op(e:GetLabelObject(),tp,eg,ep,ev,re,r,rp)
+end
+
+--EFEITO 3
+function s.check_material(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or not c:IsFaceup() then return end
+	local og=c:GetOverlayGroup()
+	if not og:IsExists(Card.IsCode,1,nil,106) then
+		Duel.Destroy(c,REASON_EFFECT)
+	end
+end
+
+function s.no_eren_cond(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsFaceup() or not c:IsLocation(LOCATION_MZONE) then return false end
+	local og=c:GetOverlayGroup()
+	return not og:IsExists(Card.IsCode,1,nil,106)
+end
+
+function s.destroy_self(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Destroy(c,REASON_EFFECT)
+end
+
