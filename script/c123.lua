070d30fceb481f79d00d49950ebcf4a85e120911
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-06 11:24:20 -0300

    Update c123.lua

--- script/c123.lua
+++ script/c123.lua
@@ -1,61 +1,69 @@
--- Progenitora
+--光の創造神 ホルアクティ (Versão Adaptada)
+--Holactie the Creator of Light (Adapted)
 local s,id=GetID()
-
--- Definindo constante de vitória por efeito (caso não esteja no core do EDOPro)
-local WIN_REASON_PROGENITORA = 0x1A  -- valor qualquer que represente vitória por efeito
-
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-
-	-- Invocação Especial: Sacrificar Titan Fundador
+	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
+	e1:SetValue(1)
 	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-
-	-- Efeito 01: Invocação não pode ser negada
+	--special summon condition
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e2:SetValue(aux.FALSE)
 	c:RegisterEffect(e2)
-
-	-- Efeito 02: Imune a todos os efeitos
+	--summon cannot be negated
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_IMMUNE_EFFECT)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetValue(s.efilter)
+	e3:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	c:RegisterEffect(e3)
-
-	-- Efeito 03: Vitória instantânea ao ser invocada
+	--win
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_DELAY)
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e4:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL) end)
-	e4:SetOperation(function(_,tp) Duel.Win(tp,0x123) end)
-
+	e4:SetOperation(s.winop)
 	c:RegisterEffect(e4)
 end
 
-function s.spfilter(c,tp)
-	return c:IsCode(121) and c:IsReleasable() and c:IsControler(tp)
+function s.spfilter(c)
+	return c:IsCode(121) -- ID da carta que deve ser sacrificada
 end
 function s.spcon(e,c)
 	if c==nil then return true end
-	return Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_MZONE,0,1,nil,c:GetControler())
+	local tp=c:GetControler()
+	local rg=Duel.GetReleaseGroup(tp)
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and rg:IsExists(s.spfilter,1,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local rg=Duel.GetReleaseGroup(tp):Filter(s.spfilter,nil)
+	if #rg>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local g=rg:Select(tp,1,1,nil)
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
-	Duel.Release(g,REASON_COST)
+	local g=e:GetLabelObject()
+	if g then
+		Duel.Release(g,REASON_COST)
+		g:DeleteGroup()
+	end
 end
-
-function s.efilter(e,te)
-	return true -- Imune a todos os efeitos
+function s.winop(e,tp,eg,ep,ev,re,r,rp)
+	local p=e:GetHandler():GetSummonPlayer()
+	Duel.Win(p,WIN_REASON_CREATORGOD)
 end
