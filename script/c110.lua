991bd687fed5f2d123917dd45d6029932950a301
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-06 10:26:10 -0300

    pieck fix

--- script/c110.lua
+++ script/c110.lua
@@ -1,14 +1,15 @@
 -- Pieck Finger
-local s,id = GetID()
+local s,id=GetID()
 function s.initial_effect(c)
-	-- Efeito: Adicionar 1 Magia Attack On Titan ao deck quando invocada normalmente ou desvirada
-	local e1 = Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND)
-	e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+	-- Quando esta carta for Invocada Normalmente ou por Flip Summon: adicione 1 Magia "Attack On Titan" do Deck à mão
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 
 	local e2 = e1:Clone()
@@ -16,25 +17,23 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
--- Filtro para buscar apenas Magias do arquétipo Attack On Titan
-function s.thfilter(c)
-	return c:IsSetCard(0x100) and c:IsType(TYPE_SPELL) and c:IsAbleToHand() -- Somente Magias Attack On Titan
+-- Filtro: apenas Magias do arquétipo 0x100 (Attack On Titan)
+function s.filter(c)
+	return c:IsSetCard(0x100) and c:IsSpell() and c:IsAbleToHand()
 end
 
--- Alvo do efeito, checando se há Magias Attack On Titan no deck
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk == 0 then 
-		return Duel.IsExistingMatchingCard(s.thfilter, tp, LOCATION_DECK, 0, 1, nil) 
-	end
-	Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK)
+-- Verifica se há Magia válida no deck
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 
--- Operação do efeito, adicionando a Magia à mão
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
-	local g = Duel.SelectMatchingCard(tp, s.thfilter, tp, LOCATION_DECK, 0, 1, 1, nil)
-	if #g > 0 then
-		Duel.SendtoHand(g, nil, REASON_EFFECT)
-		Duel.ConfirmCards(1 - tp, g)
+-- Executa a busca
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
