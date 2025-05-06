5361ab723999a82d327d393301cea309d58ffb47
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-06 12:09:37 -0300

    gil

--- script/c119.lua
+++ script/c119.lua
@@ -39,21 +39,18 @@ e4:SetOperation(s.desop)
 c:RegisterEffect(e4)
 end
 
+
 -- ===== INVOCACAO XYZ CUSTOMIZADA =====
 
 function s.matfilter(c)
-	return c:IsFaceup() and c:IsCode(109) and s.checkErenCondition(c)
+	return c:IsFaceup() and c:IsCode(103) and s.checkErenCondition(c)
 end
 
+-- Verifica se o monstro NÃO foi invocado neste turno
 function s.checkErenCondition(c)
-	local turn_id = Duel.GetTurnCount()
+	local current_turn = Duel.GetTurnCount()
 	local summon_turn = c:GetTurnID()
-	local summon_type = c:GetSummonType()
-	if (summon_type & SUMMON_TYPE_SPECIAL) == SUMMON_TYPE_SPECIAL then return true end
-	if (summon_type & SUMMON_TYPE_NORMAL) == SUMMON_TYPE_NORMAL then
-		return summon_turn < turn_id
-	end
-	return false
+	return summon_turn < current_turn
 end
 
 function s.xyzcon(e,c)
@@ -68,6 +65,7 @@ function s.xyzop(e,tp,eg,ep,ev,re,r,rp,c)
 	c:SetMaterial(g)
 	Duel.Overlay(c,g)
 end
+
 
 -- ===== CONTAGEM DE FASE FINAL DO OPONENTE =====
 
