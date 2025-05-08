059bd51b627718699a6e129f3e22d4ca65a23fa9
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-08 10:43:39 -0300

    Update c130.lua

--- script/c130.lua
+++ script/c130.lua
@@ -1,44 +1,22 @@
 -- Paradis
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Ativação
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-
-	-- ATK +300
+	--Atk
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetRange(LOCATION_FZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(s.statfilter)
-	e2:SetValue(300)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_WARRIOR|RACE_BEASTWARRIOR))
+	e2:SetValue(200)
 	c:RegisterEffect(e2)
-
-	-- DEF +300
+	--Def
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e3)
-
-	-- Imunidade contra Magias
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetCode(EFFECT_IMMUNE_EFFECT)
-	e4:SetRange(LOCATION_FZONE)
-	e4:SetTargetRange(LOCATION_MZONE,0)
-	e4:SetTarget(s.statfilter)
-	e4:SetValue(s.immval)
-	c:RegisterEffect(e4)
-end
-
--- Alvo: Guerreiro, Nível 4, do arquétipo 0x100
-function s.statfilter(e,c)
-	return c:IsFaceup() and c:IsRace(RACE_WARRIOR) and c:IsLevel(4) and c:IsSetCard(0x100)
-end
-
--- Imunidade apenas a Magias
-function s.immval(e,te)
-	return te:IsActiveType(TYPE_SPELL)
 end
