9431868d530793a7d6ad4b90ac06339bcae91b9f
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-08 11:11:08 -0300

    Update c130.lua

--- script/c130.lua
+++ script/c130.lua
@@ -14,23 +14,8 @@ function s.initial_effect(c)
 	e2:SetTarget(s.attg)
 	e2:SetValue(1000)
 	c:RegisterEffect(e2)
-
-	-- Imunidade a Magias
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_IMMUNE_EFFECT)
-	e3:SetRange(LOCATION_FZONE)
-	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e3:SetTarget(s.attg)
-	e3:SetValue(s.immval)
-	c:RegisterEffect(e3)
 end
 
 function s.attg(e,c)
 	return c:IsSetCard(0x100) and c:IsType(TYPE_XYZ)
-end
-
--- Imune a Magias
-function s.immval(e,te)
-	return te:IsActiveType(TYPE_SPELL)
 end
