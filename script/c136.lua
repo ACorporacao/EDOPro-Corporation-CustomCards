4659ee76c1c09c8cbc4872381d38453a73bc3c4b
Vinicius Reis <vinicius10079@hotmail.com>
2025-05-08 16:52:06 -0300

    Update c136.lua

--- script/c136.lua
+++ script/c136.lua
@@ -1,24 +1,26 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Destrói Monstro Xyz Rank 6 ou menor ao ser declarado alvo de ataque
+	-- Quando Levi ataca um Xyz Rank 6 ou menor, destrói imediatamente
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_BE_BATTLE_TARGET)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetCondition(s.descon)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 end
 
--- Condição: o oponente declarar ataque com monstro Xyz Rank 6 ou menor
+-- Condição: Levi ataca um monstro Xyz Rank 6 ou menor
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	local bc=Duel.GetAttacker()
-	return bc and bc:IsType(TYPE_XYZ) and bc:IsControler(1-tp) and bc:GetRank()<=6
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc and bc:IsType(TYPE_XYZ) and bc:GetRank()<=6
 end
 
--- Operação: destruir o atacante
+-- Destrói o alvo e cancela a batalha
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local bc=Duel.GetAttacker()
-	if bc:IsRelateToBattle() then
+	local bc=e:GetHandler():GetBattleTarget()
+	if bc and bc:IsRelateToBattle() then
 		Duel.Destroy(bc,REASON_EFFECT)
+		Duel.ChangeAttackTarget(nil) -- remove o alvo de ataque
 	end
 end
